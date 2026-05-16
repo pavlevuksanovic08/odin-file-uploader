dc51e72db1b24d6736081e991aa332aa0e4ef476
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-16 17:51:46 +0200

    feat: create index view and index router

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
+app.use('/', indexRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
