09eff7b42634af7142e88f8a97da3d19eb629aa0
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-16 21:38:39 +0200

    feat: add expressSession middleware with prisma-session-store

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express"
 import indexRouter from "./routes/indexRoute.js";
 import path from "path";
 import { fileURLToPath } from "url";
+import expressSession from "express-session";
+import { prisma } from "./lib/prisma.js"
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 
 const app = express();
 
@@ -11,6 +14,26 @@ const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(expressSession({
+    cookie: {
+        maxAge: 7 * 24* 60 * 60 * 1000, // 7 days
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET || "a santa at nasa",
+    store: new PrismaSessionStore(
+        prisma,
+        {
+            checkPeriod: 2* 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+     )
+    }
+));
 
 app.use('/', indexRouter)
 
