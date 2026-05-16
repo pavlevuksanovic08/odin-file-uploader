c9b67a7fdea1d936cb7b10d73cc2fbdb479c487e
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-16 19:07:45 +0200

    feat: creat e login and signup forms

---
+++ controllers/indexController.js
@@ -0,0 +1,13 @@
+
+
+export const getIndex = (req, res) => {
+    res.render("index");
+}
+
+export const getLogin = (req, res) => {
+    res.render("login")
+}
+
+export const getSignup = (req, res) => {
+    res.render("signup")
+}
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import * as contoller from "../controllers/indexController.js" 
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render("index")
-})
-
+router.get('/', contoller.getIndex)
+router.get('/signup', contoller.getSignup)
+router.get('/login', contoller.getLogin)
 export default router;
