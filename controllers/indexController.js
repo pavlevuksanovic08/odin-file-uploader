8994987f17befe738517c3c94c57e2340a627691
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-16 22:36:51 +0200

    feat: remove auth functions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,4 @@
 
-
 export const getIndex = (req, res) => {
     res.render("index");
-}
-
-export const getLogin = (req, res) => {
-    res.render("login")
-}
-
-export const getSignup = (req, res) => {
-    res.render("signup")
 }
