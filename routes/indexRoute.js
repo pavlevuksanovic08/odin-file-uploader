c6048d0ae1a25d99d45dfb66339a0c8ee9190147
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-16 22:37:16 +0200

    feat: create routers for login and signup

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import * as contoller from "../controllers/indexController.js" 
+import * as contoller from "../controllers/indexController.js"; 
+import * as auth from "../controllers/authController.js"
 
 const router = Router();
 
 router.get('/', contoller.getIndex)
-router.get('/signup', contoller.getSignup)
-router.get('/login', contoller.getLogin)
+
+router.get('/signup', auth.getSignup)
+router.post('/signup', auth.postSignup);
+
+router.get('/login', auth.getLogin);
+router.post('/login', auth.postLogin)
+
 export default router;
