404ed482c39b44709d18dfd5b5487ab32354b81f
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-19 18:25:17 +0200

    feat: add postFileUpload controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,21 @@
+import multer from "multer";
+import {fileURLToPath} from "url"
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const upload = multer({dest: path.join(__dirname, "../dist/files")});
 
 export const getIndex = (req, res) => {
     res.render("index");
 }
+
+export const postFileUpload = [ 
+    upload.single("file"),
+    (req, res) => {
+
+        if (req.file) console.log("File detetektovan."); 
+        res.redirect('/');
+    }
+]
