bf9dfd61d3ad947f057624ee940b012eccd51ce6
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-19 18:26:18 +0200

    feat: add router for file upload

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -10,6 +10,8 @@ router.get('/signup', auth.getSignup)
 router.post('/signup', auth.postSignup);
 
 router.get('/login', auth.getLogin);
-router.post('/login', auth.postLogin)
+router.post('/login', auth.postLogin);
+
+router.post('/upload', contoller.postFileUpload);
 
 export default router;
