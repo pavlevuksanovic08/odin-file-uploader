70a5a3cc7681e2f3d8fff0496cb58fd7037de5a8
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-20 11:53:03 +0200

    feat: create folder edit logic and fileName validation

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,25 +1,41 @@
 import { prisma } from "../lib/prisma.js";
+import { body, validationResult } from "express-validator";
+
+
+const folderNameValidator = [
+    body("name").trim().escape()
+    .isLength({min: 1}).withMessage("Name of the folder cant be empty string.")
+]
 
 export const getCreateFolder = (req, res) => {
     res.render("addFolder");
 }
 
-export const postCreateFolder = async (req, res, next) => {
-    try {
-        const name = req.body.name;
+export const postCreateFolder = [
+    folderNameValidator,
+    async (req, res, next) => {
+        try {
 
-        const folder = await prisma.folder.create({
-            data: {
-                name: name,
-                userId: req.session.userId
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.render("addFolder", {errors: errors.array()})
             }
-        })
+            const name = req.body.name;
 
-        res.redirect('/');
-    } catch (err) {
-        next(err)
+            const folder = await prisma.folder.create({
+                data: {
+                    name: name,
+                    userId: req.session.userId
+                }
+            })
+
+            res.redirect('/');
+        } catch (err) {
+            next(err)
+        }
     }
-}
+]
 
 export const getFolderById = async (req, res, next) => {
     try {
@@ -30,11 +46,59 @@ export const getFolderById = async (req, res, next) => {
                 id: id
             }
         })
-        if (!folder) throw new Error("There is no such a folder");
         res.render('folder', {folder});
     } catch (err) {
         next(err);
     }
-
-    
 }
+
+export const getFolderEdit = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        
+        const folder = await prisma.folder.findUnique({
+            where: {
+                id: id
+            }
+        })
+        if (!folder) throw new Error("There is no such a folder");
+
+        res.render("addFolder", {folder})
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const postFolderEdit = [
+    folderNameValidator,
+    async (req, res, next) => {
+        try {
+            const id = req.params.id;
+
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                const folder = await prisma.folder.findUnique({
+                    where: {
+                        id: id
+                    }
+                })
+                return res.render("addFolder", {folder, errors: errors.array()})
+            }
+
+            const folder = await prisma.folder.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    name: req.body.name
+                }
+            })
+            if (!folder) throw new Error("There is no such a folder");
+
+            res.redirect('/');
+        } catch (err) {
+            next(err)
+        }
+    }
+]
--- routes/folderRoute.js
+++ routes/folderRoute.js
@@ -6,4 +6,6 @@ const router = Router();
 router.get('/create', controller.getCreateFolder)
 router.post('/create', controller.postCreateFolder)
 router.get('/:id', controller.getFolderById);
+router.get('/:id/edit', controller.getFolderEdit);
+router.post('/:id/edit', controller.postFolderEdit)
 export default router
